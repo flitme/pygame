36899141d67612e223ecdd9245ca10f86a518614
flitme <ty69305@gmail.com>
2026-04-07 22:39:51 +0500

    tank_add

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 import pygame
-import time
+from tank import Tank
+# Константы для размеров поля и сетки:
+SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+GRID_SIZE = 20
+GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-MAIN_BG = (255, 0, 0)
+# Направления движения:
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
 
+# Цвет фона - черный:
+BOARD_BACKGROUND_COLOR = (0, 0, 0)
+
+# Цвет границы ячейки
+BORDER_COLOR = (93, 216, 228)
+
+# Скорость движения танка:
+SPEED = 20
+
+# Настройка игрового окна:
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+
+# Заголовок окна игрового поля:
+pygame.display.set_caption('Танчики')
+
+# Настройка времени:
+clock = pygame.time.Clock()
+
+def main():
+    """Основная функция игры."""
+    # Инициализация PyGame:
+    pygame.init()
+
+    tank = Tank()
+
+    while True:
+        clock.tick(SPEED)
+
+        tank.draw(screen)
+
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
 if __name__ == '__main__':
-    pygame.display.init()
-    pygame.font.init()
-    pygame.mouse.set_visible(False)
-    # Get size of screen and create main rendering surface.
-    size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-    screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
-    # Display splash screen.
-    screen.fill(MAIN_BG)
-    pygame.display.update()
-    time.sleep(10.0)
+    main()
--- tank.py
+++ tank.py
@@ -1,1 +1,12 @@
 # Класс танка
+import pygame 
+
+class Tank:
+    def __init__(self):
+        self.x = 200
+        self.y = 400
+        self.image = pygame.image.load('sprites/player.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (40, 40))
+    def draw(self, screen):
+        rect = pygame.Rect(self.x, self.y, 40, 40)
+        screen.blit(self.image, rect)
